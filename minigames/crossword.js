ec41e644008b435a4924534bd9df1da86d577bcf
SpacingKosmopan <spacingkosmopan.mapy@gmail.com>
2026-07-08 17:54:40 +0200

    Added two new random crosswords

--- minigames/crossword.js
+++ minigames/crossword.js
@@ -1,3 +1,11 @@
+// *****
+function getRandomIntInclusive(min, max) {
+  const minCeil = Math.ceil(min);
+  const maxFloor = Math.floor(max);
+  return Math.floor(Math.random() * (maxFloor - minCeil + 1)) + minCeil;
+}
+// *****
+
 /**
  * Array of crosswords
  * Crossword is an array of rows
@@ -16,6 +24,32 @@ const crosswords = [
     [" ", "t", " ", " ", "d", " ", " ", " ", "e", " ", " ", " ", " "],
     [" ", "e", " ", "c", "o", "m", "p", "o", "s", "i", "t", "e", " "],
     [" ", "p", " ", " ", "w", " ", " ", " ", "s", " ", " ", " ", " "],
+  ],
+  //* Crossword 2
+  [
+    ["d", "e", "a", "t", "h", "m", "a", "t", "c", "h", " ", " ", " "],
+    ["r", " ", " ", "w", " ", " ", "r", " ", " ", " ", " ", " ", " "],
+    ["a", " ", "d", "i", "v", "i", "n", "e", " ", " ", " ", "g", " "],
+    ["g", " ", " ", "n", " ", " ", "i", " ", " ", " ", " ", "u", " "],
+    ["o", " ", " ", "k", " ", "z", "e", "p", "t", "o", "l", "a", "b"],
+    ["o", " ", " ", "l", " ", " ", " ", " ", "r", " ", " ", "r", " "],
+    ["n", " ", " ", "e", " ", " ", " ", " ", "o", " ", " ", "d", " "],
+    [" ", " ", " ", " ", " ", " ", "b", "l", "o", "t", " ", " ", " "],
+    [" ", " ", " ", " ", " ", " ", " ", " ", "p", " ", " ", " ", " "],
+    [" ", " ", " ", " ", " ", "f", "i", "r", "e", "f", "l", "y", " "],
+    [" ", " ", " ", " ", " ", " ", " ", " ", "r", " ", " ", " ", " "],
+  ],
+  //* Crossword 3
+  [
+    [" ", "b", " ", " ", " ", " ", " ", " ", "f", " ", " ", "d", " "],
+    ["m", "a", "t", "c", "h", "b", "o", "x", "e", "s", " ", "r", " "],
+    [" ", "t", " ", " ", " ", " ", " ", " ", "s", " ", " ", "o", " "],
+    [" ", "t", " ", "l", "e", "v", "i", "a", "t", "h", "a", "n", " "],
+    [" ", "e", " ", " ", " ", " ", "u", "n", "i", "q", "u", "e", " "],
+    ["g", "r", "a", "v", "i", "e", "l", " ", "v", " ", " ", " ", " "],
+    [" ", "i", " ", " ", " ", " ", "s", "p", "a", "r", "k", "l", "e"],
+    [" ", "e", " ", "m", "a", "d", "b", "u", "l", "l", "e", "t", "s"],
+    [" ", "s", "a", "t", "o", "s", "h", "i", " ", " ", " ", " ", " "],
   ],
 ];
 /**
@@ -132,15 +166,224 @@ const wordsData = [
       },
     },
   ],
+  //* Crossword 2
+  [
+    // 1
+    {
+      word: "Deathmatch",
+      query: "Name of the mode where everyone has four lives",
+      direction: "horizontal",
+      startPos: {
+        x: 0,
+        y: 0,
+      },
+    },
+    // 2
+    {
+      word: "Twinkle",
+      query: "The most chaotic scout in the game",
+      direction: "vertical",
+      startPos: {
+        x: 3,
+        y: 0,
+      },
+    },
+    // 3
+    {
+      word: "Dragoon",
+      query: "A heavy tank hero who can jump over obstacles",
+      direction: "vertical",
+      startPos: {
+        x: 0,
+        y: 0,
+      },
+    },
+    // 4
+    {
+      word: "Divine",
+      query: "Name of the highest tier hero can be",
+      direction: "horizontal",
+      startPos: {
+        x: 2,
+        y: 2,
+      },
+    },
+    // 5
+    {
+      word: "Arnie",
+      query: "",
+      direction: "vertical",
+      startPos: {
+        x: 6,
+        y: 0,
+      },
+    },
+    // 6
+    {
+      word: "ZeptoLab",
+      query: "Name of the Bullet Echo developing studio",
+      direction: "horizontal",
+      startPos: {
+        x: 5,
+        y: 4,
+      },
+    },
+    // 7
+    {
+      word: "Trooper",
+      query: "Name of Satoshi's class",
+      direction: "vertical",
+      startPos: {
+        x: 8,
+        y: 4,
+      },
+    },
+    // 8
+    {
+      word: "Guard",
+      query: "Name of the red drone",
+      direction: "vertical",
+      startPos: {
+        x: 11,
+        y: 2,
+      },
+    },
+    // 9
+    {
+      word: "Firefly",
+      query: "Name of hero with the highest fire rate",
+      direction: "horizontal",
+      startPos: {
+        x: 5,
+        y: 9,
+      },
+    },
+
+    // 10
+    {
+      word: "Blot",
+      query: "It's real name is B-107",
+      direction: "horizontal",
+      startPos: {
+        x: 6,
+        y: 7,
+      },
+    },
+  ],
+  //* Crossword 3
+  [
+    // 1
+    {
+      word: "Matchboxes",
+      query: "Name of resource you collect in Sabotage mode",
+      direction: "horizontal",
+      startPos: {
+        x: 0,
+        y: 1,
+      },
+    },
+    // 2
+    {
+      word: "Festival",
+      query: "Name of event that includes 12 skins to collect, one per day",
+      direction: "vertical",
+      startPos: {
+        x: 8,
+        y: 0,
+      },
+    },
+    // 3
+    {
+      word: "Batteries",
+      query:
+        "Name of resource used to get better rewards in Team Deathmatch's Infinity Roulette",
+      direction: "vertical",
+      startPos: {
+        x: 1,
+        y: 0,
+      },
+    },
+    // 4
+    {
+      word: "Satoshi",
+      query: "Name of trooper with Force Field",
+      direction: "horizontal",
+      startPos: {
+        x: 1,
+        y: 8,
+      },
+    },
+    // 5
+    {
+      word: "Unique",
+      query: "Name of the rarest rarity tier of skins",
+      direction: "horizontal",
+      startPos: {
+        x: 6,
+        y: 4,
+      },
+    },
+    // 6
+    {
+      word: "MadBullets",
+      query: "Name of weekly giveaway on official Discord server",
+      direction: "horizontal",
+      startPos: {
+        x: 3,
+        y: 7,
+      },
+    },
+    // 7
+    {
+      word: "Sparkle",
+      query: "Name of gunner with the best healing",
+      direction: "horizontal",
+      startPos: {
+        x: 6,
+        y: 6,
+      },
+    },
+    // 8
+    {
+      word: "Leviathan",
+      query:
+        "Name of hero with the highest amount of bullets in primary weapon",
+      direction: "horizontal",
+      startPos: {
+        x: 3,
+        y: 3,
+      },
+    },
+    // 9
+    {
+      word: "Graviel",
+      query: "Name of hero with the longest hair",
+      direction: "horizontal",
+      startPos: {
+        x: 0,
+        y: 5,
+      },
+    },
+    // 10
+    {
+      word: "Drone",
+      query: "Can revive you and is not a hero",
+      direction: "vertical",
+      startPos: {
+        x: 11,
+        y: 0,
+      },
+    },
+  ],
 ];
 
 // * GAME MECHANICS * //
 
 const cellToWordsMap = {};
-
+let crosswordId = getRandomIntInclusive(0, wordsData.length - 1);
 let currentWordIndex = null;
 
-wordsData[0].forEach((wordObj, index) => {
+wordsData[crosswordId].forEach((wordObj, index) => {
   let { x, y } = wordObj.startPos;
   const len = wordObj.word.length;
 
@@ -158,7 +401,8 @@ wordsData[0].forEach((wordObj, index) => {
 
 const container = document.getElementById("crossword-container");
 const questionBox = document.getElementById("question-box");
-const gridData = crosswords[0];
+//TODO: set random
+const gridData = crosswords[crosswordId];
 
 gridData.forEach((row, y) => {
   row.forEach((letter, x) => {
@@ -191,7 +435,8 @@ function handleCellClick(x, y) {
   if (!wordIndexes || wordIndexes.length === 0) return;
 
   currentWordIndex = wordIndexes[0];
-  const wordObj = wordsData[0][currentWordIndex];
+  //TODO: set random
+  const wordObj = wordsData[crosswordId][currentWordIndex];
 
   questionBox.textContent = wordObj.query;
 
@@ -222,7 +467,8 @@ viewResultsBtn.addEventListener("click", () => {
     return;
   }
 
-  const wordObj = wordsData[0][currentWordIndex];
+  //TODO: set random
+  const wordObj = wordsData[crosswordId][currentWordIndex];
   let startX = wordObj.startPos.x;
   let startY = wordObj.startPos.y;
 
